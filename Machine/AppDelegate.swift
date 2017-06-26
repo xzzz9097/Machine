0d37e08cd29733dca4b581f88de58a6d6f882800
lyrae <lyrae@lyraes-mbp.homenet.telecomitalia.it>
2017-06-26 23:27:14 +0200

    Initial Commit

---
+++ Machine/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Machine
+//
+//  Created by lyrae on 26/06/2017.
+//  Copyright © 2017 lyrae. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Machine/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Machine
+//
+//  Created by lyrae on 26/06/2017.
+//  Copyright © 2017 lyrae. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
