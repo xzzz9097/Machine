d67db2350006c36b3355e2539b10b74809493667
xzzz9097 <xzzz9097@gmail.com>
2017-06-27 17:01:46 +0200

    Use low power mode by default

--- Machine/ViewController.swift
+++ Machine/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: NSViewController, AVCaptureVideoDataOutputSampleBufferDele
     
     var faceRect = NSView(frame: NSRect())
     
-    var lowPower = false
+    var lowPower = true
     
     var shouldTrack = true
     
